0ec692e1ad4d040e33a19ca2b498e4d8cab757ad
artesco <edipurwanto.dr@gmail.com>
2022-04-21 09:12:28 +0700

    edit ViewRemoveTodolist.php

--- View/ViewRemoveTodolist.php
+++ View/ViewRemoveTodolist.php
@@ -1,5 +1,23 @@
 <?php
 
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/RemoveTodolist.php";
+
 function viewRemoveTodolist() {
 
+    echo "Menghapus TODO" . PHP_EOL;
+
+    $pilihan = input("Nomor");
+
+    if($pilihan == "x") {
+        echo "Batal menghapus Todo" . PHP_EOL;
+    } else {
+        $success = removeTodolist($pilihan);
+
+        if ($success) {
+            echo "Sukses menghapus Todo nomor $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus Todo $pilihan" . PHP_EOL;
+        }
+    }
 }
